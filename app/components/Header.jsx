6f16356f2918359b8aed3463b827511339d6c181
kriztiank <kriztiank@gmail.com>
2023-07-12 09:55:45 +0200

    Conditional Header Links & UserButton

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,7 +1,10 @@
 import Link from 'next/link'
-// import { UserButton, auth } from '@clerk/nextjs'
+import { UserButton, auth } from '@clerk/nextjs'
 
-const Header = () => {
+const Header = async ({ username }) => {
+  const { userId } = auth()
+  console.log(userId)
+
   return (
     <nav className='flex items-center justify-between px-6 py-4 mb-5 bg-blue-700'>
       <div className='flex items-center'>
@@ -11,15 +14,34 @@ const Header = () => {
           </div>
         </Link>
       </div>
-      <>
-        <Link href='sign-in' className='text-gray-300 hover:text-white mr-4'>
-          Sign In
-        </Link>
-        <Link href='sign-up' className='text-gray-300 hover:text-white mr-4'>
-          Sign Up
-        </Link>
-      </>
+      <div className='flex items-center text-white'>
+        {!userId && (
+          <>
+            <Link
+              href='sign-in'
+              className='text-gray-300 hover:text-white mr-4'
+            >
+              Sign In
+            </Link>
+            <Link
+              href='sign-up'
+              className='text-gray-300 hover:text-white mr-4'
+            >
+              Sign Up
+            </Link>
+          </>
+        )}
+        {userId && (
+          <Link href='profile' className='text-gray-300 hover:text-white mr-4'>
+            Profile
+          </Link>
+        )}
+        <div className='ml-auto'>
+          <UserButton afterSignOutUrl='/' />
+        </div>
+      </div>
     </nav>
   )
 }
+
 export default Header
