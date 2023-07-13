e30b00db81e8c0b658004edc86e9bb10080e2c89
kriztiank <kriztiank@gmail.com>
2023-07-13 07:28:49 +0200

    themes installed

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
-// import { light } from '@clerk/themes'
+import { light } from '@clerk/themes'
 import Header from './components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -13,7 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{ baseTheme: light }}>
       <html lang='en'>
         <body className={inter.className}>
           <Header />
